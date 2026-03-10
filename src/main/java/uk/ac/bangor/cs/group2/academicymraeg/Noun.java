94acb77418d1f7b15d75011e50397164ea120621
Martyna <martynkacecot@gmail.com>
2026-03-10 22:56:02 +0000

    Noun Class

--- src/main/java/uk/ac/bangor/cs/group2/academicymraeg/Noun.java
+++ src/main/java/uk/ac/bangor/cs/group2/academicymraeg/Noun.java
@@ -4,23 +4,23 @@ import java.sql.ResultSet;
 import java.time.LocalDateTime;
 import java.util.Collection;
 
-public class Noun implements SQLObject<Noun>{
+public class Noun implements SQLObject<Noun> {
 	private long nounID;
 	private String englishWord;
 	private String welshWord;
-	public enum Gender{
-		MASCULINE,FEMININE
+
+	public enum Gender {
+		MASCULINE, FEMININE
 	}
+
 	private Gender gender;
 	private User addedBy;
 	private LocalDateTime createdAt;
 	private boolean active;
-	
-	
-	public Noun() {
-		
-	}
 
+	public Noun() {
+
+	}
 
 	public Noun(long nounID, String englishWord, String welshWord, Gender gender, User addedBy, LocalDateTime createdAt,
 			boolean active) {
@@ -34,71 +34,61 @@ public class Noun implements SQLObject<Noun>{
 		this.active = active;
 	}
 
-
 	public String getEnglishWord() {
 		return englishWord;
 	}
-
 
 	public void setEnglishWord(String englishWord) {
 		this.englishWord = englishWord;
 	}
 
-
 	public String getWelshWord() {
 		return welshWord;
 	}
-
 
 	public void setWelshWord(String welshWord) {
 		this.welshWord = welshWord;
 	}
 
-
 	public Gender getGender() {
 		return gender;
 	}
-
 
 	public void setGender(Gender gender) {
 		this.gender = gender;
 	}
 
-	//unsure if we needed this so we can always delete
+	// unsure if we needed this so we can always delete
 	public User getAddedBy() {
 		return addedBy;
 	}
 
-	//unsure if we needed this so we can always delete
+	// unsure if we needed this so we can always delete
 	public void setAddedBy(User addedBy) {
 		this.addedBy = addedBy;
 	}
 
-	//unsure if we needed this so we can always delete
+	// unsure if we needed this so we can always delete
 	public LocalDateTime getCreatedAt() {
 		return createdAt;
 	}
 
-	//unsure if we needed this so we can always delete
+	// unsure if we needed this so we can always delete
 	public void setCreatedAt(LocalDateTime createdAt) {
 		this.createdAt = createdAt;
 	}
-
 
 	public boolean isActive() {
 		return active;
 	}
 
-
 	public void setActive(boolean active) {
 		this.active = active;
 	}
 
-
 	public long getNounID() {
 		return nounID;
 	}
-
 
 	@Override
 	public Collection<Noun> fromResultSet(ResultSet rs) {
@@ -106,13 +96,11 @@ public class Noun implements SQLObject<Noun>{
 		return null;
 	}
 
-
 	@Override
 	public String toDeleteSQL() {
 		// TODO Auto-generated method stub
 		return null;
 	}
-
 
 	@Override
 	public String toInsertSQL() {
@@ -120,19 +108,15 @@ public class Noun implements SQLObject<Noun>{
 		return null;
 	}
 
-
 	@Override
 	public String toUpdateSQL() {
 		// TODO Auto-generated method stub
 		return null;
 	}
 
-
 	@Override
 	public String toString() {
 		return "Noun [nounID=" + nounID + ", englishWord=" + englishWord + ", welshWord=" + welshWord + ", gender="
 				+ gender + ", addedBy=" + addedBy + ", createdAt=" + createdAt + ", active=" + active + "]";
 	}
-	
-	
 }
