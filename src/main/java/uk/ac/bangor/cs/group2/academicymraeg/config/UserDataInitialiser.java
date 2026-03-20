c416f77ba8781222de4b35ce8ce9fba87cc46308
Becca D <81300493+BeccaDunnington@users.noreply.github.com>
2026-03-20 14:46:19 +0000

    Stop tracking application.properties

--- src/main/java/uk/ac/bangor/cs/group2/academicymraeg/config/UserDataInitialiser.java
+++ src/main/java/uk/ac/bangor/cs/group2/academicymraeg/config/UserDataInitialiser.java
@@ -1,31 +1,31 @@
-package uk.ac.bangor.cs.group2.academicymraeg.config;
-
-import org.springframework.boot.CommandLineRunner;
-import org.springframework.context.annotation.Bean;
-import org.springframework.context.annotation.Configuration;
-import org.springframework.security.crypto.password.PasswordEncoder;
-
-import uk.ac.bangor.cs.group2.academicymraeg.models.User;
-import uk.ac.bangor.cs.group2.academicymraeg.models.User.Role;
-import uk.ac.bangor.cs.group2.academicymraeg.repository.UserRepository;
-
-@Configuration
-public class UserDataInitialiser {
-
-	@Bean
-	CommandLineRunner userDatainitialiser(UserRepository userRepository, PasswordEncoder passwordEncoder) {
-		return args -> {
-			User user = new User();
-			user.setUsername("testStudent");
-			user.setPasswordHash(passwordEncoder.encode("password"));
-			user.setEmail("student@test.com");
-			user.setFirstName("Bob");
-			user.setLastName("Bobson");
-			user.setRole(Role.STUDENT);
-
-			userRepository.save(user);
-
-			System.out.println("User saved to DB:" + user);
-		};
-	}
-}
+//package uk.ac.bangor.cs.group2.academicymraeg.config;
+//
+//import org.springframework.boot.CommandLineRunner;
+//import org.springframework.context.annotation.Bean;
+//import org.springframework.context.annotation.Configuration;
+//import org.springframework.security.crypto.password.PasswordEncoder;
+//
+//import uk.ac.bangor.cs.group2.academicymraeg.models.User;
+//import uk.ac.bangor.cs.group2.academicymraeg.models.User.Role;
+//import uk.ac.bangor.cs.group2.academicymraeg.repository.UserRepository;
+//
+//@Configuration
+//public class UserDataInitialiser {
+//
+//	@Bean
+//	CommandLineRunner userDatainitialiser(UserRepository userRepository, PasswordEncoder passwordEncoder) {
+//		return args -> {
+//			User user = new User();
+//			user.setUsername("testStudent");
+//			user.setPasswordHash(passwordEncoder.encode("password"));
+//			user.setEmail("student@test.com");
+//			user.setFirstName("Bob");
+//			user.setLastName("Bobson");
+//			user.setRole(Role.STUDENT);
+//
+//			userRepository.save(user);
+//
+//			System.out.println("User saved to DB:" + user);
+//		};
+//	}
+//}
