e57be8d2cc223babe066ed6b13b3c649116c7f20
Summer2906 <Summer.louise2906@gmail.com>
2026-03-29 19:04:24 +0100

    landing page first draft - no permissions

--- src/main/java/uk/ac/bangor/cs/group2/academicymraeg/controller/GreetingController.java
+++ src/main/java/uk/ac/bangor/cs/group2/academicymraeg/controller/GreetingController.java
@@ -1,17 +1,19 @@
 package uk.ac.bangor.cs.group2.academicymraeg.controller;
 
+import org.springframework.security.core.Authentication;
+import org.springframework.security.core.context.SecurityContextHolder;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.bind.annotation.RequestParam;
 
 @Controller
 public class GreetingController {
 
-	@GetMapping("/")
-	public String greeting(@RequestParam(name = "name", required = false, defaultValue = "World") String name,
-			Model model) {
-		model.addAttribute("name", name);
-		return "greeting";
-	}
+    @GetMapping("/")
+    public String landing(Model model) {
+    	Authentication auth = SecurityContextHolder.getContext().getAuthentication();
+    	String username = auth.getName();
+    	model.addAttribute("username", username);
+        return "landing-page";
+    }
 }
