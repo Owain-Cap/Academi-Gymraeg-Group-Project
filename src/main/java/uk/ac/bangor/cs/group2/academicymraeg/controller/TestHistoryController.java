3dc1a9a99f5c9606d90612b8235eea4a8538a963
Martyna <martynkacecot@gmail.com>
2026-04-02 22:16:02 +0100

    Added a view for the old tests with showing opition a and b if its for the gender their answer and if its correct or not doesn't show right answer

--- src/main/java/uk/ac/bangor/cs/group2/academicymraeg/controller/TestHistoryController.java
+++ src/main/java/uk/ac/bangor/cs/group2/academicymraeg/controller/TestHistoryController.java
@@ -1,22 +1,28 @@
 package uk.ac.bangor.cs.group2.academicymraeg.controller;
 
+import java.util.List;
+
 import org.springframework.security.core.Authentication;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
 
 import uk.ac.bangor.cs.group2.academicymraeg.models.Test;
+import uk.ac.bangor.cs.group2.academicymraeg.models.TestQuestions;
+import uk.ac.bangor.cs.group2.academicymraeg.repository.TestQuestionRepository;
 import uk.ac.bangor.cs.group2.academicymraeg.repository.TestRepository;
-
-import java.util.List;
 
 @Controller
 public class TestHistoryController {
 
     private final TestRepository testRepository;
+	private TestQuestionRepository testQuestionRepository;
+    
 
-    public TestHistoryController(TestRepository testRepository) {
+    public TestHistoryController(TestRepository testRepository, TestQuestionRepository testQuestionRepository) {
         this.testRepository = testRepository;
+        this.testQuestionRepository = testQuestionRepository;
     }
 
     @GetMapping("/my-tests")
@@ -25,5 +31,24 @@ public class TestHistoryController {
         List<Test> tests = testRepository.findByUsernameOrderByCreatedAtDesc(username);
         model.addAttribute("tests", tests);
         return "test-history";
+    }
+    
+    
+    @GetMapping("/review-test/{testId}")
+    public String reviewTest(@PathVariable Long testId, Authentication authentication, Model model) {
+        String username = authentication.getName();
+        Test test = testRepository.findById(testId)
+        	    .orElseThrow(() -> new IllegalArgumentException("Test not found"));
+
+        if (!test.getUsername().equals(username)) {
+            throw new IllegalArgumentException("Not allowed");
+        }
+
+        // Load test questions
+        List<TestQuestions> questions = testQuestionRepository.findByTestOrderByPositionAsc(test);
+
+        model.addAttribute("test", test);
+        model.addAttribute("questions", questions);
+        return "review-test";
     }
 }
