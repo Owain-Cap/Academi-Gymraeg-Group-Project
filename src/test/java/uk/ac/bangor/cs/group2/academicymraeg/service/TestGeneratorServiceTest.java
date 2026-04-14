386aa75f99eb144a71060956a92bf56782914d7f
Becca D <81300493+BeccaDunnington@users.noreply.github.com>
2026-04-14 20:54:58 +0100

    TestGeneratorServiceTest unit test added

---
+++ src/test/java/uk/ac/bangor/cs/group2/academicymraeg/service/TestGeneratorServiceTest.java
@@ -0,0 +1,34 @@
+package uk.ac.bangor.cs.group2.academicymraeg.service;
+
+import static org.junit.jupiter.api.Assertions.*;
+
+import org.junit.jupiter.api.Test;
+
+class TestGeneratorServiceTest {
+
+	@Test
+	void testGenerateTestForUser() {
+		fail("Not yet implemented");
+	}
+
+	@Test
+	void testGetTestById() {
+		fail("Not yet implemented");
+	}
+
+	@Test
+	void testSubmitTest() {
+		fail("Not yet implemented");
+	}
+
+	@Test
+	void testHasActiveTest() {
+		fail("Not yet implemented");
+	}
+
+	@Test
+	void testGetActiveTest() {
+		fail("Not yet implemented");
+	}
+
+}
