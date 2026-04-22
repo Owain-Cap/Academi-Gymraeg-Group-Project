bf71e66e704d90bee7c17daf2e84ee7a95affe57
Sam <SamRobertsJones@outlook.com>
2026-04-22 18:42:22 +0100

    Java doc for initial login controller

--- src/main/java/uk/ac/bangor/cs/group2/academicymraeg/controller/LoginController.java
+++ src/main/java/uk/ac/bangor/cs/group2/academicymraeg/controller/LoginController.java
@@ -3,9 +3,18 @@ package uk.ac.bangor.cs.group2.academicymraeg.controller;
 import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.GetMapping;
 
+/**
+ * Controller class for mapping requests for the login page.
+ */
 @Controller
 public class LoginController {
 
+	/**
+	 * Handles HTTP GET requests for the "/login" URL.
+	 * Displays the login page.
+	 *
+	 * @return The name of the Thymeleaf template to render ("login").
+	 */
 	@GetMapping("/login")
 	public String login() {
 		return "login";
